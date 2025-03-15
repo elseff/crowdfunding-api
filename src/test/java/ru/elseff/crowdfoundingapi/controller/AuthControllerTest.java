89baf5428e783ef37a67f88cb243f65335bae76c
elseff <danilapetrov522@gmail.com>
2025-03-15 17:32:05 +0400

    unit tests

---
+++ src/test/java/ru/elseff/crowdfoundingapi/controller/AuthControllerTest.java
@@ -0,0 +1,43 @@
+package ru.elseff.crowdfoundingapi.controller;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("Тестирование авторизации и регистрации")
+class AuthControllerTest {
+    @Test
+    @DisplayName("Тестирование регистрации, если неверный email")
+    void test1() {
+
+    }
+
+    @Test
+    @DisplayName("Тестирование регистрации, если email уже существует")
+    void test2() {
+
+    }
+
+    @Test
+    @DisplayName("Тестирование успешной регистрации")
+    void test3() {
+
+    }
+
+    @Test
+    @DisplayName("Тестирование авторизации, если пользователь не найден")
+    void test4() {
+
+    }
+
+    @Test
+    @DisplayName("Тестирование авторизации, если неверный пароль")
+    void test5() {
+
+    }
+
+    @Test
+    @DisplayName("Тестирование успешной авторизации")
+    void test6() {
+
+    }
+}
---
+++ src/test/java/ru/elseff/crowdfoundingapi/controller/UserControllerTest.java
@@ -0,0 +1,27 @@
+package ru.elseff.crowdfoundingapi.controller;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserControllerTest {
+
+    @Test
+    @DisplayName("Тестирование получения списка пользователей")
+    void test1() {
+
+    }
+
+    @Test
+    @DisplayName("Тестирование получения пользователя по id, если не найден")
+    void test2() {
+
+    }
+
+    @Test
+    @DisplayName("Тестирование успешного получения пользователя по id")
+    void name() {
+
+    }
+}
