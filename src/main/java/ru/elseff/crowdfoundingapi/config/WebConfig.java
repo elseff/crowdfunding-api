0dac518c5bc45d74bbef089e7b6f871b4c9ea674
elseff <danilapetrov522@gmail.com>
2025-05-19 16:11:23 +0400

    add maven CI

--- src/main/java/ru/elseff/crowdfoundingapi/config/WebConfig.java
+++ src/main/java/ru/elseff/crowdfoundingapi/config/WebConfig.java
@@ -1,5 +1,7 @@
 package ru.elseff.crowdfoundingapi.config;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.multipart.MultipartResolver;
@@ -7,16 +9,25 @@ import org.springframework.web.multipart.support.StandardServletMultipartResolve
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.List;
+
+@Slf4j
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
+    @Value("#{'${allowed.origins}'.split(' ')}")
+    List<String> origins;
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*") // Разрешить все источники
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*");
+                .allowedOrigins(origins.toArray(new String[0]))
+                .allowedHeaders("*")
+                .allowedMethods("*")
+                .exposedHeaders("Content-Type", "Authorization");
+        log.info("Allowed origins --- " + String.join(", ", origins));
     }
+
     @Bean
     public MultipartResolver multipartResolver() {
         return new StandardServletMultipartResolver();
